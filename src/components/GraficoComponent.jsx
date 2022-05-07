eb3382798ebcef7175d300f3be08d2c967b583f6
akiofabio <fabio.ikenoue@fatec.sp.gov.br>
2022-05-07 06:21:24 -0300

    up

--- src/components/GraficoComponent.jsx
+++ src/components/GraficoComponent.jsx
@@ -1,6 +1,24 @@
 import React, { useEffect , useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, BarController, BarElement, Title , CategoryScale, LinearScale } from 'chart.js';
 
+ChartJS.register( BarController , BarElement , Title ,  CategoryScale , LinearScale);
+var dataChart = {
+    labels: ['Jun', 'Jul', 'Aug'],
+    datasets: [
+      {
+        id: 1,
+        label: '',
+        data: [5],
+      },
+      {
+        id: 2,
+        label: '',
+        data: [3],
+      },
+    ],
+  }
 function GraficoComponent(){
-    
+    return <Bar data={dataChart} />
 }
 export default GraficoComponent
